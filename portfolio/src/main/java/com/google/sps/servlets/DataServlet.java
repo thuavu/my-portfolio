7ef0d88be7e15886539db4f356d2289db81ea266
Thu <thuvu2100@gmail.com>
2020-06-15 01:06:18 -0500

    Remove commented-out lines

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -28,8 +28,6 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    //response.setContentType("text/html;");
-    //response.getWriter().println("<h1>Hello Thu!</h1>");
     response.setContentType("application/json;");
 
     ArrayList<String> listValues = new ArrayList<String>(); 
